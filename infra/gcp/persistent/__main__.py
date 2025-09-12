192b4ff0fa83f2fdbba57d18848eeb0bc05add08
Launa Greer <greerlauna@gmail.com>
2025-09-12 18:56:01 -0500

    Attempt to fix Cloud Workflow permissions

--- infra/gcp/persistent/__main__.py
+++ infra/gcp/persistent/__main__.py
@@ -1076,7 +1076,7 @@ gcp.storage.BucketIAMMember(
     role="roles/storage.objectCreator",
     member=f"serviceAccount:{cloudsql_service_agent_email}",
     opts=pulumi.ResourceOptions(
-        depends_on=[enabled_services, cloudsql_service_agent],
+        depends_on=[*enabled_services, cloudsql_service_agent],
         provider=gcp_provider,
     ),
 )
@@ -1090,7 +1090,7 @@ gcp.storage.BucketIAMMember(
         "serviceAccount:", cloudsql_service_agent.email
     ),
     opts=pulumi.ResourceOptions(
-        depends_on=[enabled_services, cloudsql_service_agent],
+        depends_on=[*enabled_services, cloudsql_service_agent],
         provider=gcp_provider,
     ),
 )
@@ -1103,7 +1103,7 @@ gcp.storage.BucketIAMMember(
     role="roles/storage.legacyBucketWriter",
     member=f"serviceAccount:{cloudsql_service_agent_email}",
     opts=pulumi.ResourceOptions(
-        depends_on=[enabled_services, cloudsql_service_agent],
+        depends_on=[*enabled_services, cloudsql_service_agent],
         provider=gcp_provider,
     ),
 )
@@ -1116,7 +1116,7 @@ gcp.storage.BucketIAMMember(
     role="roles/storage.admin",
     member=f"serviceAccount:{cloudsql_service_agent_email}",
     opts=pulumi.ResourceOptions(
-        depends_on=[enabled_services, cloudsql_service_agent],
+        depends_on=[*enabled_services, cloudsql_service_agent],
         provider=gcp_provider,
     ),
 )
